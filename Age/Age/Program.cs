b1ea64bc3bd52f1e9c2f3fd669ebc3cd5ef79c45
SKhorramzadeh <s.khorramzadeh@gmail.com>
2024-09-29 20:28:55 +0330

    Add files via upload

---
+++ Age/Age/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Age;
+            int intAge;
+            Console.WriteLine("Enter your age ...");
+            Age = Console.ReadLine();
+            intAge = int.Parse(Age);
+            if (intAge>18)
+            {
+                Console.WriteLine("Your age is passed");
+            }
+            else
+            {
+                Console.WriteLine("Your age is not passed");
+            }
+        }
+    }
+}
